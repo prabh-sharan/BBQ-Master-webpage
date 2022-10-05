a39c339fd715bc5db5404caecc39020059ffe823
Prabh Sharan Singh <95855341+prabh-sharan@users.noreply.github.com>
2022-10-05 16:53:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ emailCollector.addEventListener("submit", event =>{
     let fname= ourFormInfo.get("firstName")
     let emailId= ourFormInfo.get("emailAddress")
 
-    let updatedInfo =`            
+    let updatedInfo =      
     <h2>Congratulations ${fname} </h2>
     <p>You're are on your way to become a BBQ master! </p>
     <p>You will receive updates on - ${emailId}</p>
    
 
     <p class="fine-print">We'll never share your information
-     without your permission</p>`
+     without your permission</p>
 
     let updateMainContent= document.getElementById("updateMain")
     updateMainContent.innerHTML = updatedInfo
